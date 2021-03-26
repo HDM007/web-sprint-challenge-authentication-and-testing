81e6cd03e58ad8b9a5ee4bb3c4a9956fa85ee716
hdm007 <harry.mardirossian13@gmail.com>
2021-03-26 06:26:30 -0400

    added basic lifecycle testing methods

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,23 @@
+const request = require("supertest");
+const db = require("../data/dbConfig");
+const server = require("./server");
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db("users").truncate();
+});
+
+afterAll(async () => {
+  await db.destroy();
+})
+
+
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(true)
 })
+
