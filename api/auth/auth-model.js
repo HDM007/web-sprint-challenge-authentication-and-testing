505833f4d6b6f5ad3cc60981b87eba7e4f32cc6c
hdm007 <harry.mardirossian13@gmail.com>
2021-03-26 04:45:59 -0400

    model file outlined

---
+++ api/auth/auth-model.js
@@ -0,0 +1,24 @@
+const db = require('../../data/dbConfig.js');
+
+function getUser () {
+
+}
+
+function findByUsername () {
+
+}
+
+function findById () {
+
+}
+
+function createUser () {
+    
+}
+
+module.exports = {
+    getUser,
+    findByUsername,
+    findById,
+    createUser
+}
