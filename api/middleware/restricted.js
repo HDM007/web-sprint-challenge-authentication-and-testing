f138517303a777fb3edd7f15853b10da493b1364
hdm007 <harry.mardirossian13@gmail.com>
2021-03-26 06:01:52 -0400

    added restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,26 @@
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
-  next();
+  try {
+    const token= req.headers.authorization
+    if(!token) {
+      return res.status(401).json({
+      message: "token required"
+      })
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=>{
+    if(err) {
+      return res.status(401).json({
+        message: "token invalid"
+      })
+    }
+    req.token= decoded
+
+    next();
+    })
+  }  catch(err) {
+    next(err)
+  }
   /*
     IMPLEMENT
 
